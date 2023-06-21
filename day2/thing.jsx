15d49a3da5e6a7913983db56dfa4be0f1fe43cfe
sanjm3tr <HP233041@M301-CC0126.local>
2023-06-21 13:55:16 +0800

    ok

--- day2/thing.jsx
+++ day2/thing.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Alert, Button ,  SafeAreaView,
-    ScrollView, TouchableOpacity} from 'react-native';
+    ScrollView, TouchableOpacity, Pressable} from 'react-native';
 import {Image} from "expo-image"
 import { Entypo } from '@expo/vector-icons';
 // import img from "./assets/download.jpeg"
@@ -14,10 +14,10 @@ export default function App() {
       <Text style={{fontWeight: "400", fontSize: 18,}}>Some guy</Text>
 {/* </View> */}
       <Text style={{marginLeft: 150}}>7 seconds ago</Text>
-      <Image source={require('./assets/download.jpeg')} onPress={() => {
-        Alert.alert("you pressed on profile pic")}} style={{width: 60, height: 60, flex: 1, marginBottom: 1, borderRadius: 500}} />
+      {/* <Pressable onPress={() => {
+        Alert.alert("you pressed on profile pic")}} style={{width: 60, height: 60, flex: 1, marginBottom: 1, borderRadius: 500}}> */}
+      <Image source={require('./assets/download.jpeg')}  />
       <TouchableOpacity activeOpacity={0.1}>
-
       <Entypo name="dots-three-vertical" size={24} color="black" style={{marginLeft:"90%"}} onPress={() => {Alert.alert("Yo wsup")}} />
       </TouchableOpacity>
       {/* <Button onPress={() => {Alert.alert("Yo wsup")}} title=":" /> */}
